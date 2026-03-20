ca2994957846c9465717bafe7e48f73ec3ab5fda
alanfbs <allanfbs2@gmail.com>
2026-03-19 21:26:57 -0300

    updated migrations folder

--- migration_password.sql
+++
@@ -1,6 +0,0 @@
--- migration_password.sql
-ALTER TABLE public.user_info 
-  ADD COLUMN IF NOT EXISTS password_hash text;
-
--- Se desejar remover o login_pin antigo:
--- ALTER TABLE public.user_info DROP COLUMN IF EXISTS login_pin;
